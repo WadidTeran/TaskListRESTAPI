b3ff7ed310b5518a59eb33a7bc28b2445aee55a6
Terán Wadid <wadidjt@gmail.com>
2023-07-08 18:02:30 -0500

    Add methods to JwtUtils
    - Add extractAllClaims method in JwtUtils
    - Add isTokenValid in JwtUtils
    - Add getClaim in JwtUtils
    - Add getUsernameFromToken in JwtUtils

--- src/main/java/org/kodigo/proyectos/tasklist/security/jwt/utils/JwtUtils.java
+++ src/main/java/org/kodigo/proyectos/tasklist/security/jwt/utils/JwtUtils.java
@@ -1,5 +1,6 @@
 package org.kodigo.proyectos.tasklist.security.jwt.utils;
 
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Component;
 
 import java.security.Key;
 import java.util.Date;
+import java.util.function.Function;
 
 @Component
 @Slf4j
@@ -32,5 +34,32 @@ public class JwtUtils {
         .setExpiration(new Date(System.currentTimeMillis() + Long.parseLong(timeExpiration)))
         .signWith(getSignatureKey(), SignatureAlgorithm.HS256)
         .compact();
+  }
+
+  public Claims extractAllClaims(String token) {
+    return Jwts.parserBuilder()
+        .setSigningKey(getSignatureKey())
+        .build()
+        .parseClaimsJws(token)
+        .getBody();
+  }
+
+  public boolean isTokenValid(String token) {
+    try {
+      extractAllClaims(token);
+      return true;
+    } catch (Exception e) {
+      log.error("Invalid token, error: ".concat(e.getMessage()));
+      return false;
+    }
+  }
+
+  public <T> T getClaim(String token, Function<Claims, T> claimsTFunction) {
+    Claims claims = extractAllClaims(token);
+    return claimsTFunction.apply(claims);
+  }
+
+  public String getUsernameFromToken(String token) {
+    return getClaim(token, Claims::getSubject);
   }
 }
