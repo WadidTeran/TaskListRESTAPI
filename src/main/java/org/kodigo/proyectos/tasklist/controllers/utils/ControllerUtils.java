9a916a99b3e4534a755bf5d8a89b3dc24d9a2c24
Terán Wadid <wadidjt@gmail.com>
2023-07-08 18:54:37 -0500

    Add ControllerUtils and delete CredentialsValidator

---
+++ src/main/java/org/kodigo/proyectos/tasklist/controllers/utils/ControllerUtils.java
@@ -0,0 +1,22 @@
+package org.kodigo.proyectos.tasklist.controllers.utils;
+
+import org.kodigo.proyectos.tasklist.entities.UserEntity;
+import org.kodigo.proyectos.tasklist.repositories.UserRepository;
+import org.kodigo.proyectos.tasklist.security.jwt.utils.JwtUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+public class ControllerUtils {
+  @Autowired private UserRepository userRepository;
+  @Autowired private JwtUtils jwtUtils;
+
+  public UserEntity getUserEntityFromHeaders(HttpHeaders headers) {
+    String token = headers.getFirst("Authorization").substring(7);
+    String username = jwtUtils.getUsernameFromToken(token);
+    return userRepository.findByUsername(username).orElseThrow();
+  }
+}
