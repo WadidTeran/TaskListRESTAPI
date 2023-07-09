6d8ce7e3753325d1294b6b2e5bc8541fcf3bd8a7
Terán Wadid <wadidjt@gmail.com>
2023-07-08 19:00:41 -0500

    Modify TaskController
    - Change @RequestBody to @RequestHeaders to get UserEntity from headers

--- src/main/java/org/kodigo/proyectos/tasklist/controllers/TaskController.java
+++ src/main/java/org/kodigo/proyectos/tasklist/controllers/TaskController.java
@@ -3,10 +3,12 @@ package org.kodigo.proyectos.tasklist.controllers;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import lombok.AllArgsConstructor;
+import org.kodigo.proyectos.tasklist.controllers.utils.ControllerUtils;
 import org.kodigo.proyectos.tasklist.entities.Task;
 import org.kodigo.proyectos.tasklist.entities.UserEntity;
 import org.kodigo.proyectos.tasklist.services.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,11 +20,12 @@ import java.util.Optional;
 @RequestMapping("/tasks")
 public class TaskController {
   @Autowired private TaskService taskService;
+  @Autowired private ControllerUtils controllerUtils;
 
   @Operation(summary = "Gets a list of tasks according to query params")
   @GetMapping
   public ResponseEntity<List<Task>> getTaskList(
-      @RequestBody UserEntity user,
+      @RequestHeader HttpHeaders headers,
       @Parameter(description = "Task status query param: {completed, pending}") @RequestParam
           String status,
       @Parameter(description = "Task \"due to\" query param: {today, previous, future}")
@@ -33,6 +36,7 @@ public class TaskController {
           String rel,
       @Parameter(description = "Task category name query param") @RequestParam(required = false)
           String category) {
+    UserEntity user = controllerUtils.getUserEntityFromHeaders(headers);
     Optional<List<Task>> optTaskList = taskService.getTaskList(user, status, due, rel, category);
     if (optTaskList.isPresent()) {
       List<Task> tasks = optTaskList.get();
--- src/main/java/org/kodigo/proyectos/tasklist/controllers/utils/ControllerUtils.java
+++ src/main/java/org/kodigo/proyectos/tasklist/controllers/utils/ControllerUtils.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
-
 @Component
 public class ControllerUtils {
   @Autowired private UserRepository userRepository;
