f3636de6f3e24e7d55301d34e03369448aaa454b
Andres Johan Florez Gonzalez <89665982+johanflorez98@users.noreply.github.com>
2023-07-10 01:08:18 -0500

    Add deleteTask test

--- src/test/java/org/kodigo/proyectos/tasklist/controllers/TaskControllerTest.java
+++ src/test/java/org/kodigo/proyectos/tasklist/controllers/TaskControllerTest.java
@@ -3,13 +3,24 @@ package org.kodigo.proyectos.tasklist.controllers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.kodigo.proyectos.tasklist.entities.Category;
+import org.kodigo.proyectos.tasklist.entities.Relevance;
+import org.kodigo.proyectos.tasklist.entities.Task;
+import org.kodigo.proyectos.tasklist.entities.UserEntity;
+import org.kodigo.proyectos.tasklist.services.CategoryService;
 import org.kodigo.proyectos.tasklist.services.TaskService;
+import org.kodigo.proyectos.tasklist.services.UserService;
 import org.kodigo.proyectos.tasklist.testutils.TestHelper;
+import org.kodigo.proyectos.tasklist.testutils.TestUser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.*;
+
+import java.time.LocalDate;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,6 +32,8 @@ class TaskControllerTest {
   @Autowired private TaskService taskService;
   @LocalServerPort private int port;
   @Autowired private TestHelper testHelper;
+  @Autowired private UserService userService;
+  @Autowired private CategoryService categoryService;
 
   @BeforeEach
   void setUp() {
@@ -54,5 +67,28 @@ class TaskControllerTest {
 
   @DisplayName("DELETE /tasks/{taskId}")
   @Test
-  void deleteTask() {}
+  void deleteTask() {
+    testHelper.registerTestUser(testRestTemplate);
+    testHelper.createTestData();
+    HttpHeaders headers = testHelper.getHeadersWithAuthenticationForTestUser(testRestTemplate);
+    HttpEntity<String> request = new HttpEntity<>(null, headers);
+
+    UserEntity userEntity = userService.getUserByUsername(TestUser.USERNAME.value).orElseThrow();
+
+    Long id = 1L;
+
+    Task task = taskService.findByUserAndTaskId(userEntity, id).orElseThrow();
+
+    ResponseEntity<Task> response =
+            testRestTemplate.exchange(
+                    BASE_URL.concat(String.format("/%d", id)), HttpMethod.DELETE, request, Task.class);
+
+    assertEquals(HttpStatus.NO_CONTENT, response.getStatusCode());
+
+    Optional<Task> deletedTask =
+            taskService.findByUserAndTaskId(userEntity,id);
+
+    assertTrue(deletedTask.isEmpty());
+    testHelper.deleteTestUser();
+  }
 }
